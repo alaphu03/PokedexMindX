2f43b70e72e7dcbf07d036b0867e565f1b6d540d
alaphu03 <phunguyen2004@gmail.com>
2019-08-10 12:09:37 +0700

    pull test3

--- app.js
+++ app.js
@@ -24,19 +24,33 @@ function fetchPokemons() {
     });
 };
 
-// function setUpEvents() {
-//     var btnSearch = document.getElementById("btn_search");
-//     btnSearch.addEventListener("click", function(e){
-//     var searchBar = document.getElementById("search_bar");
-//     var searchString = searchBar.value;
-//     var fullURL=`https://pokeapi.co/api/v2/pokemon/${searchString}/`;
-//     ;
-//     sendGetRequest(fullURL, function(responseData){
-//         var pokemons = pokemons;
-//         renderCharacters(pokemons)
-//     })
-// });      
-// }
+function setUpEvents() {
+    var btnSearch = document.getElementById("btn_search");
+    btnSearch.addEventListener("click", function(e){
+    var searchBar = document.getElementById("search_bar");
+    var searchString = searchBar.value;
+    var url=`https://pokeapi.co/api/v2/pokemon/${searchString}`;
+    ;
+    sendGetRequest(url, function(pokemonsSearch){
+        var pokemonsSearch= pokemonsSearch.forms[0];
+        renderPokemon(pokemonsSearch);
+    })
+});      
+}
+function renderPokemon(pokemonsSearch) {
+    var pokemonName = pokemonsSearch.name;
+    var content = document.getElementById("content");
+    content.textContent = ""
+    var pokemonimg = "https://img.pokemondb.net/artwork/"+pokemonsSearch.name+".jpg";
+    var pokemonHTML=`
+    <div>
+        <img src = ${pokemonimg}>
+        <h2>${pokemonName}</h2>
+    </div>
+    `
+    content.insertAdjacentHTML("beforeend", pokemonHTML);
+    ;
+};
 fetchPokemons() ;
-// setUpEvents() ; 
+setUpEvents() ; 
 
