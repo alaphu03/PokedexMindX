c149bc2c8fc97f2e76339ea0af43991c2615e32f
alaphu03 <phunguyen2004@gmail.com>
2019-08-13 20:55:22 +0700

    Git Pull 13/8/2019

--- app.js
+++ app.js
@@ -12,8 +12,10 @@ function renderPokemons(pokemons) {
         <div id="pokemonCard">
             <img id = PokemonIMG src = ${pokemonimg}>
             <div class= "container">
-                <h3>${pokemonIndex}</h3>
-                <h2>${pokemonName}</h2>
+                <span>
+                    <h3 id="pokemonIndex">#${pokemonIndex}.</h3>
+                    <h2>${pokemonName}</h2>
+                </span>
             </div>
         </div>
         `
@@ -49,15 +51,29 @@ function renderPokemon(pokemonsSearch) {
     var pokemonimg = "https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/"+pokemonID+".png?raw=true"
     // var pokemonimg = "https://img.pokemondb.net/sprites/sun-moon/icon/"+pokemonName+".png"
     var pokemonHTML=`
-    <div>
-        <h3 id = "pokemonID">${pokemonID}</h3>
-        <img id = PokemonIMG src = ${pokemonimg}>
-        <h2 id="pokemonName">${pokemonName}</h2>
+    <div id="pokemonCard">
+        <img id = cardIMG src = ${pokemonimg}>
+        <h2 id="cardTitle">${pokemonID}.${pokemonName}</h2>
     </div>
     `
     content.insertAdjacentHTML("beforeend", pokemonHTML);
     ;
 };
+// function setUpEvents2() {
+//     var btnSearch = document.getElementById("content");
+//     btnSearch.addEventListener("click", function(e){
+    // var searchBar = document.getElementById("search_bar");
+    // var searchString = searchBar.value;
+    // var key = marvelKey(privateKey, publicKey);
+    // var fullURL=`${url}?${key}&nameStartsWith=${searchString}`;
+    // sendGetRequest(fullURL, function(responseData){
+    //     var characters = responseData.data.results;
+    //     renderCharacters(characters)
+    // })
+//     console.log('clicked')
+// });      
+// }
 fetchPokemons() ;
 setUpEvents() ; 
+// setUpEvents2();
 
