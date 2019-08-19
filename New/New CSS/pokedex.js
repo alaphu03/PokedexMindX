516c507df54cc37742dd5abf807683bd937aabf1
alaphu03 <phunguyen2004@gmail.com>
2019-08-19 22:58:05 +0700

    pull 19/8

---
+++ New/New CSS/pokedex.js
@@ -0,0 +1,44 @@
+function newTab(){
+    var NT1 = document.getElementById("Home")
+    NT1.addEventListener("click", function(a){
+        window.open("pokedex.html", "_self")
+    })
+    var NT3 = document.getElementById("About")
+    NT3.addEventListener("click", function(c){
+        window.open("pokedex.html", "_self")
+    })
+    var NT4 = document.getElementById("Source_Code")
+    NT4.addEventListener("click", function(d){
+        window.open("pokedex.html", "_self")
+    })
+    var PKDTab = document.getElementById("generation")
+    PKDTab.addEventListener("click", function(e){
+        window.open("NationalPokedex.html", "_self")
+    })
+    var PKDTab1 = document.getElementById("generation_1")
+    PKDTab.addEventListener("click", function(f){
+        window.open("Generation1.html", "_self")
+    })
+    var PKDTab2 = document.getElementById("generation_2")
+    PKDTab.addEventListener("click", function(g){
+        window.open("Generation2.html", "_self")
+    })
+    var PKDTab3 = document.getElementById("generation_3")
+    PKDTab.addEventListener("click", function(h){
+        window.open("Generation3.html", "_self")
+    })
+    var PKDTab4 = document.getElementById("generation_4")
+    PKDTab.addEventListener("click", function(j){
+        window.open("Generation4.html", "_self")
+    })
+    var PKDTab5 = document.getElementById("generation_5")
+    PKDTab.addEventListener("click", function(k){
+        window.open("Generation5.html", "_self")
+    })
+    var PKDTab6 = document.getElementById("generation_6")
+    PKDTab.addEventListener("click", function(i){
+        window.open("Generation6.html", "_self")
+    })
+    
+}
+newTab();
