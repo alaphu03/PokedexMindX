d923616ab69e6cf740e1f7f9782b378693c37204
alaphu03 <phunguyen2004@gmail.com>
2019-08-20 22:18:39 +0700

    git pull 20/8

--- New/New CSS/pokedex.js
+++ New/New CSS/pokedex.js
@@ -1,44 +1,61 @@
-// function newTab(){
-//     var NT1 = document.getElementById("Home")
-//     NT1.addEventListener("click", function(a){
-//         window.open("pokedex.html", "_self")
+var url = "https://pokeapi.co/api/v2/pokedex/1";
+function renderPokemons(Pokemons) {
+    var content = document.getElementById("content")
+    content.textContent=""
+    for (var i = 0; i <Pokemons.length; i++){
+        var Pokemon = Pokemons[i];
+        var pokemonID = Pokemon.entry_number;
+        var pokemon_name = Pokemon.pokemon_species.name;
+        if (Pokemon.pokemon_species.name == "nidoran-m"){
+            var pokemonHTML=`
+            <div id="PokemonCard">
+                <div id="img_container">
+                    <img id="img" src="${imgSrc}">
+                </div>
+                <div id="Pokemon_container">
+                    <h2 id="PokemonID">${pokemonID}</h2>
+                    <h3 id="PokemonName">${pokemon_name}</h3>
+                </div>
+            </div>
+            `;
+            content.insertAdjacentHTML("beforeend", pokemonHTML);
+        }
+        else {
+            var imgSrc = 'https://www.pkparaiso.com/imagenes/xy/sprites/animados/'+pokemon_name+'.gif';
+            var pokemonHTML=`
+            <div id="PokemonCard">
+                <div id="img">
+                    <img  src="${imgSrc}">
+                </div>
+                <div id="Pokemon_container">
+                    <h2 id="PokemonID">#${pokemonID}</h2>
+                    <h3 id="PokemonName">.${pokemon_name}</h3>
+                </div>
+            </div>
+            `;
+            content.insertAdjacentHTML("beforeend", pokemonHTML);
+        }
+    }
+}   
+function fetchPokemons(){
+    var fullURL = `${url}`;
+    sendGetRequest(fullURL, function(Pokemonss) {
+        var Pokemons = Pokemonss.pokemon_entries;
+        renderPokemons(Pokemons)
+    });
+}
+// function setUpEvents() {
+//     var btnSearch = document.getElementById("btn_search");
+//     btnSearch.addEventListener("click", function(e){
+//     var searchBar = document.getElementById("search_bar");
+//     var searchString = searchBar.value;
+//     var key = marvelKey(privateKey, publicKey);
+//     var fullURL=`${url}?${key}&nameStartsWith=${searchString}`;
+//     sendGetRequest(fullURL, function(responseData){
+//         var Pokemons = responseData.data.results;
+//         renderPokemons(Pokemons)
 //     })
-//     var NT3 = document.getElementById("About")
-//     NT3.addEventListener("click", function(c){
-//         window.open("pokedex.html", "_self")
-//     })
-//     var NT4 = document.getElementById("Source_Code")
-//     NT4.addEventListener("click", function(d){
-//         window.open("pokedex.html", "_self")
-//     })
-//     var PKDTab = document.getElementById("generation")
-//     PKDTab.addEventListener("click", function(e){
-//         window.open("NationalPokedex.html", "_self")
-//     })
-//     var PKDTab1 = document.getElementById("generation_1")
-//     PKDTab.addEventListener("click", function(f){
-//         window.open("Generation1.html", "_self")
-//     })
-//     var PKDTab2 = document.getElementById("generation_2")
-//     PKDTab.addEventListener("click", function(g){
-//         window.open("Generation2.html", "_self")
-//     })
-//     var PKDTab3 = document.getElementById("generation_3")
-//     PKDTab.addEventListener("click", function(h){
-//         window.open("Generation3.html", "_self")
-//     })
-//     var PKDTab4 = document.getElementById("generation_4")
-//     PKDTab.addEventListener("click", function(j){
-//         window.open("Generation4.html", "_self")
-//     })
-//     var PKDTab5 = document.getElementById("generation_5")
-//     PKDTab.addEventListener("click", function(k){
-//         window.open("Generation5.html", "_self")
-//     })
-//     var PKDTab6 = document.getElementById("generation_6")
-//     PKDTab.addEventListener("click", function(i){
-//         window.open("Generation6.html", "_self")
-//     })
-    
+// });      
 // }
-// newTab();
+fetchPokemons();
+// setUpEvents();
