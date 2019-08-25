9af0344f3d25c82f96f274e5fd04d0e7f1fe721b
alaphu03 <phunguyen2004@gmail.com>
2019-08-25 23:03:47 +0700

    git pull 25/8

--- Test5/detail.js
+++ Test5/detail.js
@@ -1,51 +1,83 @@
 var id= window.location.search.substring(4)
-console.log(id);
 
-var url = "https://pokeapi.co/api/v2/pokedex/1/?offset=0&limit=20/${id}";
-
-function renderPokemons(Pokemons){
-    console.log(Pokemons);
+var url = "https://pokeapi.co/api/v2/pokemon/";
+function renderPokemons(PokemonCard){
     
-    var content = document.getElementById("dt_content")
+    var content = document.getElementById("content")
     content.textContent = ""
-    var pokemonID = Pokemons.entry_number;
-    var pokemon_name = Pokemons.pokemon_species.name;
-    console.log(Pokemons.pokemon_species.url);
-    
-    console.log(pokemon_name);
-    
-    var imgSrc = 'https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/'+pokemon_name+'.png';
+    var pokemonID = PokemonCard.id;
+    // console.log(Pokemon)
+    var pokemon_name = PokemonCard.forms[0].name;
+    var pokemon_height = (((PokemonCard.height *0.328084)+0.001)*100/100).toFixed(1); 
+    var pokemon_weight = (((PokemonCard.weight *0.220462)+0.001)*100/100).toFixed(2);
+    var pokemontype = PokemonCard.types;
+    var pokemonabi = PokemonCard.abilities;
+    var imgSrc = 'https://img.pokemondb.net/artwork/'+pokemon_name+'.jpg'
     var pokemonHTML=`
-    <div>
-        
-            <img id="img" src="${imgSrc}">
-        
-            <small id="PokemonID">#${pokemonID}</small>
+    <div id='Card'>
+        <img id="pokemonIMG" src=${imgSrc}>
+        <h1 id="IDContainer">
+            <div id="ID">#${pokemonID}.</div>
+            <div id="name">${pokemon_name}</div>
             <br>
-            <small id="PokemonName">${pokemon_name}</small>
-            <br>
- 
+        </h1>
+        <div id="Height">
+            <div id="H">Height:</div>
+            <div id="HH">${pokemon_height} ft</div>
+        </div>
+        <div id="Weight">
+            <div id="W">Weight:</div>
+            <div id="WW">${pokemon_weight} lbs</div>
+        </div>
+        <div id="T">Type:
+        </div>
+        <div id="A">Ability:
+        </div>
     </div>
+    `;
+    content.insertAdjacentHTML("afterbegin", pokemonHTML);
+    for (var type= 0; type< pokemontype.length; type++){
+        var content2 =document.getElementById("T")
+        var pokemonTYPE =pokemontype[type].type.name
+        var typeHTML=`
+        <div id="Type">${pokemonTYPE}</div>
+        `
+        content2.insertAdjacentHTML("beforeend", typeHTML)
+    }
+    for (var abi= 0; abi< pokemonabi.length; abi++){
+        var content3 =document.getElementById("A")
+        var pokemonABI =pokemonabi[abi].ability.name
+        var abiHTML=`
+        <div id="ABI">${pokemonABI}</div>
+        `
+        content3.insertAdjacentHTML("beforeend", abiHTML)
+//     var pokemonHTML=`
+//     <div>
+        
+//             <img id="img" src="${imgSrc}">
+        
+//             <small id="PokemonID">#${pokemonID}</small>
+//             <br>
+//             <small id="PokemonName">${pokemon_name}</small>
+//             <br>
+ 
+//     </div>
     
-        `;
-        content.insertAdjacentHTML("beforeend", pokemonHTML);
-
+//         `;
+//         content.insertAdjacentHTML("beforeend", pokemonHTML);
+    }
 }
 function fetchCharacter () {
 
-    var fullUrl = `${url}?${id}`
+    var pokemonURL = `${url}${id}`
+    sendGetRequest(pokemonURL, function (responseData) {
 
-    //3. Send request, handle response data
-    sendGetRequest(fullUrl, function (responseData) {
-
-            var Pokemons = responseData.pokemon_entries[id-1];
-            // console.log(Pokemons);
+            var PokemonCard = responseData;
             
-            renderPokemons(Pokemons)
+            renderPokemons(PokemonCard)
         
 
     })
 }
 
 fetchCharacter()
-// console.log("kjkljkl");
